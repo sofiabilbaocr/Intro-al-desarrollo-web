cea6b6559da3960b6ef0078b9be7f180861c2ad0
Paula Sofia Bilbao Cruz <sofia.bilbao@ufm.edu>
2026-02-04 09:32:58 -0600

    Exercise 5 Button for contact info and dark mode updated

--- entregables/script.js
+++ entregables/script.js
@@ -20,7 +20,7 @@ document.getElementById('saludo').innerText = saludo;
 
 
 
-//EJERCICIO 2: SALUDO
+//EJERCICIO 2: OCULTAR EXPERIENCIA LABORAL
 document.getElementById("hideexperience").onclick = function () { //cuando hagan clic en el boton con id de hideexperience en este boton, ejecuta esto
   document.getElementById("experiencialaboral").classList.toggle("d-none");          //classList es la parte del elemento HTML que deja ver y cambiar las clases desde JavaScript en esye caso a dn
                                                                                     //el toggle es como un ON/OFF
@@ -30,16 +30,100 @@ document.getElementById("hideexperience").onclick = function () { //cuando hagan
 
 
 //EJERCICIO 3: MODO
-const toggleButton = document.getElementById('modo');
-const body = document.body;
+document.getElementById("modo").onclick = function () {
+  const body = document.body;
+  const edu = document.getElementById("educacion");
+  const exp = document.getElementById("experiencialaboral");
+  const lenguajes = document.getElementById("lenguajes");
+  const softskills = document.getElementById("softskills");
+  const idiomas = document.getElementById("idiomas");
+  const contacto = document.getElementById("contacto");
+  const quiensoy = document.getElementById("quiensoy");
+  const tEdu = document.getElementById("tedu");
+  const tExp = document.getElementById("texp");
+  const btnmodo = document.getElementById("modo");
+  const btnhideexperience = document.getElementById("hideexperience");
+  const btnhidecontact = document.getElementById("hidecontact");
 
-toggleButton.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-  // Guardar preferencia (opcional)
-  if (body.classList.contains('dark-mode')) {
-    localStorage.setItem('theme', 'dark');
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+  
+    //Fondo
+    body.classList.add("bg-dark", "text-white");
+    body.classList.remove("bg-light");
+  
+    
+    
+    // Botón
+    btnmodo.classList.remove("btn-outline-success");
+    btnmodo.classList.add("btn-light");
+    btnhideexperience.classList.remove("btn-outline-success");
+    btnhideexperience.classList.add("btn-light");
+    btnhidecontact.classList.remove("btn-outline-success");
+    btnhidecontact.classList.add("btn-light");
+
+
+
+    // Cajas (sections)
+    edu.classList.add("bg-dark", "text-white");
+    exp.classList.add("bg-dark", "text-white");
+    lenguajes.classList.add("bg-dark", "text-white");
+    softskills.classList.add("bg-dark", "text-white");
+    idiomas.classList.add("bg-dark", "text-white");
+    contacto.classList.add("bg-dark", "text-white");
+    quiensoy.classList.add("bg-dark", "text-white");
+
+
+
+    // Tablas
+    tEdu.classList.remove("table-success", "table-striped-columns");
+    tEdu.classList.add("table-dark");
+
+    tExp.classList.remove("table-success", "table-striped-columns");
+    tExp.classList.add("table-dark");
+
+
+
   } else {
-    localStorage.setItem('theme', 'light');
-  }
-});
+    // Botón
+    btnmodo.classList.remove("btn-light");
+    btnmodo.classList.add("btn-outline-success");
+    btnhideexperience.classList.remove("btn-light");
+    btnhideexperience.classList.add("btn-outline-success");
+    btnhidecontact.classList.remove("btn-light");
+    btnhidecontact.classList.add("btn-outline-success");
 
+    // Cajas
+    edu.classList.remove("bg-dark", "text-white");
+    exp.classList.remove("bg-dark", "text-white");
+    lenguajes.classList.remove("bg-dark", "text-white");
+    softskills.classList.remove("bg-dark", "text-white");
+    idiomas.classList.remove("bg-dark", "text-white");
+    contacto.classList.remove("bg-dark", "text-white");
+    quiensoy.classList.remove("bg-dark", "text-white");
+
+
+    // Tablas
+    tEdu.classList.remove("table-dark");
+    tEdu.classList.add("table-success", "table-striped-columns");
+
+    tExp.classList.remove("table-dark");
+    tExp.classList.add("table-success", "table-striped-columns");
+
+    //Fondo
+    body.classList.remove("bg-dark", "text-white");
+    body.classList.add("bg-light");
+
+
+  }
+};
+
+
+//EJERCICIO 2: OCULTAR Informacion de contacto
+document.getElementById("hidecontact").onclick = function () { //cuando hagan clic en el boton con id de hideexperience en este boton, ejecuta esto
+document.getElementById("contacto").classList.toggle("d-none");          //classList es la parte del elemento HTML que deja ver y cambiar las clases desde JavaScript en esye caso a dn
+                                                                                    //el toggle es como un ON/OFF
+                                                                                    //d none es display none
+
+};
