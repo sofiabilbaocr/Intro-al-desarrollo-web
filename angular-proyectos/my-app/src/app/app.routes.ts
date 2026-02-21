741f8c4166d40ab496805552f5bd299bec057645
Paula Sofia Bilbao Cruz <sofia.bilbao@ufm.edu>
2026-02-21 17:21:44 -0600

    Exercise 1. Basic Routing

--- angular-proyectos/my-app/src/app/app.routes.ts
+++ angular-proyectos/my-app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Experience} from '../components/experience/experience';
+import { Hero } from '../components/hero/hero';;
+import { Skils } from '../components/skils/skils';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'hero', pathMatch: 'full' },
+
+  { path: 'hero', component: Hero },
+  { path: 'skills', component: Skils },
+
+  { path: '**', redirectTo: 'about' }
+];
