e18f48f35c909bf0101fbb6bf4d0c89b8977982d
Paula Sofia Bilbao Cruz <sofia.bilbao@ufm.edu>
2026-03-14 00:13:56 -0600

    Exercise 3. Advice API created

--- angular-proyectos/my-app/src/components/projects/projects.ts
+++ angular-proyectos/my-app/src/components/projects/projects.ts
@@ -13,9 +13,26 @@ export class Projects implements OnInit {
 
   constructor(private githubService: GithubService) {}
 
-  ngOnInit(): void {
-    this.githubService.getRepos().subscribe(data => {
-      this.repos = data;
-    });
-  }
+ngOnInit(): void {
+  this.repos = [
+    {
+      name: 'Final Project Board Game',
+      stargazers_count: 0,
+      description: 'Juego de mesa desarrollado en Python',
+      html_url: 'https://github.com/alejandro-ufm/final-project-board-game-sofiabilbaoc'
+    },
+    {
+      name: 'Parcial No.2 Pokemon Battle',
+      stargazers_count: 0,
+      description: 'Batalla de Pokemon en Python',
+      html_url: 'https://github.com/alejandro-ufm/parcial-no-2-pok-mon-battle-sofiabilbaoc'
+    },
+    {
+      name: 'Lab No.5 The Aurora Zoo',
+      stargazers_count: 0,
+      description: 'Simulación de zoológico en C#',
+      html_url: 'https://github.com/alejandro-ufm/lab-no-5-the-aurora-zoo-sofiabilbaoc'
+    }
+  ];
+}
 }
